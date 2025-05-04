6280de9bb48f133be95981bb20171fd56bd57dbb
Artemy-sweden <ghosteagle2020@mail.ru>
2025-05-04 19:36:42 +0300

    laba-3 done

--- src/App.js
+++ src/App.js
@@ -1,8 +1,30 @@
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./pages/Home";
+import About from "./pages/About";
+import Menu from "./pages/Menu";
+import Reviews from "./pages/Reviews";
+import Contacts from "./pages/Contacts";
+import Header from "./components/Header";
+import Footer from "./components/Footer";
 
+import "./styles/app.css";
+import Cart from "./pages/Cart";
+import { CartProvider } from "./utils/useCart";
 
 function App() {
   return (
-    <h1>Hello bod</h1>
+    <CartProvider>
+      <Header />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/menu" element={<Menu />} />
+        <Route path="/reviews" element={<Reviews />} />
+        <Route path="/contacts" element={<Contacts />} />
+        <Route path="/cart" element={<Cart />} />
+      </Routes>
+      <Footer />
+    </CartProvider>
   );
 }
 
---
+++ src/pages/About.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import AboutSection from "../components/AboutSection";
+
+const About = () => {
+  return (
+    <main className="wrapper">
+      <AboutSection />
+    </main>
+  );
+};
+
+export default About;
